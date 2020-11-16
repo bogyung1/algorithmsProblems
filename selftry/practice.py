5cbe875c5f78182bea8c73848040e3995cbd2207
user <bogyung1@naver.com>
2020-11-16 22:26:35 +0900

    rearrange

--- selftry/practice.py
+++ selftry/practice.py
@@ -5,15 +5,15 @@ def sumArray(n, s):
         result=result+s[i]
     return result
 
-list = [1,2,3,4,5,6,7,8,9,10];
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 n=len(list)
 sumArray(n,list);
 
-#두줄로 마무으리
+# 두줄로 마무으리
 result=sum(list)
 print(result)
 
-#교환정렬, 비내림차순(오름차순)
+# 교환정렬, 비내림차순(오름차순)
 def exchangeSort(n, s):
     for i in range(n):
         j=i+1
@@ -24,6 +24,6 @@ def exchangeSort(n, s):
                 s[j]=temp
 
                 print(s)
-n=5
+n= 5
 s=[1,3,2,6,7]
-exchangeSort(n,s)
+exchangeSort(n, s)
