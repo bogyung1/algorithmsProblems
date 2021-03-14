a067bbbf79c166fc2f0a296f6941845eb579fec9
user <bogyung1@naver.com>
2021-03-14 23:30:31 +0900

    modify

--- baekjoon/p1021.py
+++ baekjoon/p1021.py
@@ -15,7 +15,7 @@ for i in range(m):
                 del q[0]
                 cnt += 1
     else:
-        while True:
+        while True: 
             if q[0] == s[i]:
                 del q[0]
                 break
